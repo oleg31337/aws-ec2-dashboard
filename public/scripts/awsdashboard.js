ba94d8176cd9078d3ed1ee3c8feb639feb86bf8e
Oleg31337 <oleg31337@yahoo.com>
2021-05-24 17:31:52 -0400

    re-wrote most of the functions to use fetch instead of XMLHttpRequest removed yet unnecessary md5 function

--- public/scripts/awsdashboard.js
+++ public/scripts/awsdashboard.js
@@ -6,8 +6,6 @@ var activedimmer='\
     </div>'
 var globalconfig={}; //global app config
 
-var MD5 = function(d){var r = M(V(Y(X(d),8*d.length)));return r.toLowerCase()};function M(d){for(var _,m="0123456789ABCDEF",f="",r=0;r<d.length;r++)_=d.charCodeAt(r),f+=m.charAt(_>>>4&15)+m.charAt(15&_);return f}function X(d){for(var _=Array(d.length>>2),m=0;m<_.length;m++)_[m]=0;for(m=0;m<8*d.length;m+=8)_[m>>5]|=(255&d.charCodeAt(m/8))<<m%32;return _}function V(d){for(var _="",m=0;m<32*d.length;m+=8)_+=String.fromCharCode(d[m>>5]>>>m%32&255);return _}function Y(d,_){d[_>>5]|=128<<_%32,d[14+(_+64>>>9<<4)]=_;for(var m=1732584193,f=-271733879,r=-1732584194,i=271733878,n=0;n<d.length;n+=16){var h=m,t=f,g=r,e=i;f=md5_ii(f=md5_ii(f=md5_ii(f=md5_ii(f=md5_hh(f=md5_hh(f=md5_hh(f=md5_hh(f=md5_gg(f=md5_gg(f=md5_gg(f=md5_gg(f=md5_ff(f=md5_ff(f=md5_ff(f=md5_ff(f,r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+0],7,-680876936),f,r,d[n+1],12,-389564586),m,f,d[n+2],17,606105819),i,m,d[n+3],22,-1044525330),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+4],7,-176418897),f,r,d[n+5],12,1200080426),m,f,d[n+6],17,-1473231341),i,m,d[n+7],22,-45705983),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+8],7,1770035416),f,r,d[n+9],12,-1958414417),m,f,d[n+10],17,-42063),i,m,d[n+11],22,-1990404162),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+12],7,1804603682),f,r,d[n+13],12,-40341101),m,f,d[n+14],17,-1502002290),i,m,d[n+15],22,1236535329),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+1],5,-165796510),f,r,d[n+6],9,-1069501632),m,f,d[n+11],14,643717713),i,m,d[n+0],20,-373897302),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+5],5,-701558691),f,r,d[n+10],9,38016083),m,f,d[n+15],14,-660478335),i,m,d[n+4],20,-405537848),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+9],5,568446438),f,r,d[n+14],9,-1019803690),m,f,d[n+3],14,-187363961),i,m,d[n+8],20,1163531501),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+13],5,-1444681467),f,r,d[n+2],9,-51403784),m,f,d[n+7],14,1735328473),i,m,d[n+12],20,-1926607734),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+5],4,-378558),f,r,d[n+8],11,-2022574463),m,f,d[n+11],16,1839030562),i,m,d[n+14],23,-35309556),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+1],4,-1530992060),f,r,d[n+4],11,1272893353),m,f,d[n+7],16,-155497632),i,m,d[n+10],23,-1094730640),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+13],4,681279174),f,r,d[n+0],11,-358537222),m,f,d[n+3],16,-722521979),i,m,d[n+6],23,76029189),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+9],4,-640364487),f,r,d[n+12],11,-421815835),m,f,d[n+15],16,530742520),i,m,d[n+2],23,-995338651),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+0],6,-198630844),f,r,d[n+7],10,1126891415),m,f,d[n+14],15,-1416354905),i,m,d[n+5],21,-57434055),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+12],6,1700485571),f,r,d[n+3],10,-1894986606),m,f,d[n+10],15,-1051523),i,m,d[n+1],21,-2054922799),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+8],6,1873313359),f,r,d[n+15],10,-30611744),m,f,d[n+6],15,-1560198380),i,m,d[n+13],21,1309151649),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+4],6,-145523070),f,r,d[n+11],10,-1120210379),m,f,d[n+2],15,718787259),i,m,d[n+9],21,-343485551),m=safe_add(m,h),f=safe_add(f,t),r=safe_add(r,g),i=safe_add(i,e)}return Array(m,f,r,i)}function md5_cmn(d,_,m,f,r,i){return safe_add(bit_rol(safe_add(safe_add(_,d),safe_add(f,i)),r),m)}function md5_ff(d,_,m,f,r,i,n){return md5_cmn(_&m|~_&f,d,_,r,i,n)}function md5_gg(d,_,m,f,r,i,n){return md5_cmn(_&f|m&~f,d,_,r,i,n)}function md5_hh(d,_,m,f,r,i,n){return md5_cmn(_^m^f,d,_,r,i,n)}function md5_ii(d,_,m,f,r,i,n){return md5_cmn(m^(_|~f),d,_,r,i,n)}function safe_add(d,_){var m=(65535&d)+(65535&_);return(d>>16)+(_>>16)+(m>>16)<<16|65535&m}function bit_rol(d,_){return d<<_|d>>>32-_}
-
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
@@ -79,6 +77,10 @@ function filterTable() { // function to filter table
   }
 }
 
+function errorTableContents(errmsg){
+  document.getElementById("TableContents").innerHTML ='<div class="ui inverted placeholder segment"><div class="ui inverted icon header"><i class="exclamation triangle icon"></i>'+errmsg+'</div></div>';
+  $('#TableDimmer').removeClass('active');  
+}
 async function getTableData(account,region){ // get instance and volumes data from the backend
   console.log('getTableData');
   var stat=fetch("describeinstancestatus?region="+region+"&account="+account);
@@ -91,13 +93,11 @@ async function getTableData(account,region){ // get instance and volumes data fr
           generateTable(ss.value,ii.value,vv.value,account,region);
         }).catch(err=>{
           console.log(err);
-          document.getElementById("TableContents").innerHTML ='<div class="ui inverted placeholder segment"><div class="ui inverted icon header"><i class="exclamation triangle icon"></i>Error getting data for this region</div></div>';
-          $('#TableDimmer').removeClass('active');
+          errorTableContents('Error getting data for this region');
         });
     }).catch(err=>{
           console.log(err);
-          document.getElementById("TableContents").innerHTML ='<div class="ui inverted placeholder segment"><div class="ui inverted icon header"><i class="exclamation triangle icon"></i>Error getting data for this region</div></div>';
-          $('#TableDimmer').removeClass('active');
+          errorTableContents('Error getting data for this region');
         });
 }
 function clearTableData(){
@@ -107,18 +107,16 @@ function clearTableData(){
 
 function getGlobalConfiguration(nextfunction){ // get global app config from the backend
   console.log('getGlobalConfiguration');
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.timeout=5000;
-  var url = "configuration";
-  xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      window.globalconfig=JSON.parse(this.responseText);
-
+  fetch('configuration')
+    .then(resp => resp.json())
+    .then(config => {
+      window.globalconfig=config;
       if (typeof(nextfunction) == 'function') {nextfunction()};
-    };
-  };
-  xmlhttp.open("GET", url, true);
-  xmlhttp.send();
+    })
+    .catch(err=>{
+      errorTableContents('Error getting global configuration.</br>Internal server error.');
+      console.error(err);
+    });
 }
 
 function generateMenu(account,region){ // build menu with accounts and regions
@@ -196,14 +194,11 @@ function logoutDialog(){ // log-out confirmation dialog
 
 function checkSession() { // check if user is authenticated and there is a valid session on the backend.
   console.log("checkSession");
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.timeout=5000;
-  var url = "checksession";
-  xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      var response = JSON.parse(this.responseText); //convert reply to json
-      if (typeof response.message != 'undefined' && response.message=="ok" && typeof response.username != 'undefined'){
-        document.getElementById('logintext').innerHTML=response.username;
+  fetch("checksession")
+    .then(resp=>resp.json())
+    .then(session=>{
+      if (typeof session.message != 'undefined' && session.message=="ok" && typeof session.username != 'undefined'){
+        document.getElementById('logintext').innerHTML=session.username;
         document.getElementById('loginmenu').setAttribute("onclick", "logoutDialog()");
         document.getElementById('loginmenu').removeAttribute("href");
       }
@@ -212,10 +207,10 @@ function checkSession() { // check if user is authenticated and there is a valid
         document.getElementById('loginmenu').removeAttribute("onclick");
         document.getElementById('loginmenu').setAttribute("href", "login");
       }
-    }
-  };
-  xmlhttp.open("GET", url, true);
-  xmlhttp.send();
+    })
+    .catch(err=>{
+      console.error(err);
+    });
 }
 
 function generateTable(statuses,instances,volumes,account,awsregion) { // create instances table contents
@@ -353,6 +348,7 @@ function responseDialog(message,error){ // response and error dialog to confirm 
   }
   $('.ui.mini.modal').modal('show');
 }
+
 function doManage(account,region,instanceId,command){ // send a manage command to backend app
   console.log("doManage");
   var xmlhttp = new XMLHttpRequest();
@@ -380,51 +376,43 @@ function doManage(account,region,instanceId,command){ // send a manage command t
   $('.ui.mini.modal').modal('hide'); //hide modal
   document.getElementById('modal_contents').innerHTML=''; //destroy modal contents
 }
+
 function displayConsoleScreenshot(instanceId,account,awsregion){ // show instance console screenshot dialog
-    document.getElementById('modal_contents').setAttribute("class", "ui large modal");
-    document.getElementById('modal_contents').setAttribute("style", "background-color:#4d4d4d; color:#f2f2f2");
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.timeout=10000;
-    var url = "consolescreenshot?InstanceId="+instanceId+"&region="+awsregion+"&account="+account;
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-        var contents='<div class="ui image content" style="background-color:#4d4d4d; color:#f2f2f2">\
-            <img class="ui centered fluid image" src="data:image/jpg;base64, '+this.responseText+'">\
-            </div>\
-            <div class="ui actions" style="background-color:#4d4d4d; color:#f2f2f2">\
-                <div class="ui black bottom right icon button" onclick="displayConsoleScreenshot(\''+instanceId+'\',\''+account+'\',\''+awsregion+'\')"><i class="sync icon"></i>&nbsp;Refresh</div>\
-                <div class="ui black deny bottom right button">Close</div>\
-            </div>';
-            document.getElementById('modal_contents').innerHTML=contents;
-            }
-    };
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
-    document.getElementById('modal_contents').innerHTML=activedimmer+'</br><div class="actions"><div class="ui black deny right button">Close</div></div>;';
-    $('.ui.large.modal').modal('show');
+  console.log("displayConsoleScreenshot");
+  document.getElementById('modal_contents').setAttribute("class", "ui large modal");
+  document.getElementById('modal_contents').setAttribute("style", "background-color:#4d4d4d; color:#f2f2f2");
+  fetch("consolescreenshot?InstanceId="+instanceId+"&region="+awsregion+"&account="+account)
+    .then(resp=>resp.text())
+    .then(screenshot64=>{
+      var contents='<div class="ui image content" style="background-color:#4d4d4d; color:#f2f2f2">\
+        <img class="ui centered fluid image" src="data:image/jpg;base64, '+screenshot64+'">\
+        </div>\
+        <div class="ui actions" style="background-color:#4d4d4d; color:#f2f2f2">\
+            <div class="ui black bottom right icon button" onclick="displayConsoleScreenshot(\''+instanceId+'\',\''+account+'\',\''+awsregion+'\')"><i class="sync icon"></i>&nbsp;Refresh</div>\
+            <div class="ui black deny bottom right button">Close</div>\
+        </div>';
+      document.getElementById('modal_contents').innerHTML=contents;
+    });
+  document.getElementById('modal_contents').innerHTML=activedimmer+'</br><div class="actions"><div class="ui black deny right button">Close</div></div>;';
+  $('.ui.large.modal').modal('show');
 }
 
 function displayConsoleOutput(instanceId,account,awsregion){ // show instance console log dialog
-    document.getElementById('modal_contents').setAttribute("class", "ui large modal");
-    document.getElementById('modal_contents').setAttribute("style", "background-color:#4d4d4d; color:#f2f2f2");
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.timeout=10000;
-    var url = "consoleoutput?InstanceId="+instanceId+"&region="+awsregion+"&account="+account;
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            var contents='<div class="header" style="background-color:#4d4d4d; color:#f2f2f2">Console output</div>\
-            <div class="scrolling content" style="background-color:#4d4d4d; color:#f2f2f2"><p style="background-color:#1a1a1a; color:#e6e6e6">'+atob(this.responseText).split("\n").join("<br />")+'"</p></div>\
-            <div class="actions" style="background-color:#4d4d4d; color:#f2f2f2">\
-                <div class="ui black bottom right icon button" onclick="displayConsoleOutput(\''+instanceId+'\',\''+account+'\',\''+awsregion+'\')"><i class="sync icon"></i>&nbsp;Refresh</div>\
-                <div class="ui black deny right button">Close</div>\
-            </div>';
-            document.getElementById('modal_contents').innerHTML=contents;
-            }
-    };
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
-    document.getElementById('modal_contents').innerHTML=activedimmer+'</br><div class="actions"><div class="ui black deny right button">Close</div></div>;';
-    $('.ui.large.modal').modal('show');
+  document.getElementById('modal_contents').setAttribute("class", "ui large modal");
+  document.getElementById('modal_contents').setAttribute("style", "background-color:#4d4d4d; color:#f2f2f2");
+  fetch("consoleoutput?InstanceId="+instanceId+"&region="+awsregion+"&account="+account)
+    .then(resp=>resp.text())
+    .then(responseText=>{
+      var contents='<div class="header" style="background-color:#4d4d4d; color:#f2f2f2">Console output</div>\
+        <div class="scrolling content" style="background-color:#4d4d4d; color:#f2f2f2"><p style="background-color:#1a1a1a; color:#e6e6e6">'+atob(responseText).split("\n").join("<br />")+'"</p></div>\
+        <div class="actions" style="background-color:#4d4d4d; color:#f2f2f2">\
+            <div class="ui black bottom right icon button" onclick="displayConsoleOutput(\''+instanceId+'\',\''+account+'\',\''+awsregion+'\')"><i class="sync icon"></i>&nbsp;Refresh</div>\
+            <div class="ui black deny right button">Close</div>\
+        </div>';
+      document.getElementById('modal_contents').innerHTML=contents;
+    });
+  document.getElementById('modal_contents').innerHTML=activedimmer+'</br><div class="actions"><div class="ui black deny right button">Close</div></div>;';
+  $('.ui.large.modal').modal('show');
 }
 
 function appInit() { // main app that is called from the html page to initialize the frontend app
